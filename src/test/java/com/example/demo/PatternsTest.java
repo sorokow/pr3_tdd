265ec727b185f78669d542f0bee628f8d6128a6e
sorokow <sorokov_a02@mail.ru>
2021-10-10 18:30:49 +0300

    added test getParrent()

--- src/test/java/com/example/demo/PatternsTest.java
+++ src/test/java/com/example/demo/PatternsTest.java
@@ -7,8 +7,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class PatternsTest {
+    Patterns patterns = new Patterns();
 
     @Test
     void getPattern() {
+        assert patterns.getPattern(0) == 0;
+        assert patterns.getPattern(1) == 1;
+        assert patterns.getPattern(2) == 2;
+        assert patterns.getPattern(3) == 3;
+        assert patterns.getPattern(4) == 4;
+        assert patterns.getPattern(5) == 5;
+        assert patterns.getPattern(6) == 6;
+
     }
 }
